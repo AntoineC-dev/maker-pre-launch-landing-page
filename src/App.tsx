81f3ceadbde1b14940b77f44a6bac8b51623f742
Antoine Cheminat <ac.dev@hotmail.com>
2023-09-30 10:57:22 -0400

    Create products layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import styles from './App.module.css';
-import { Header } from './layouts';
+import { Header, Products } from './layouts';
 
 function App() {
   return (
     <div className={styles.wrapper}>
       <Header />
+      <Products />
     </div>
   );
 }
---
+++ src/layouts/Products/Products.tsx
@@ -0,0 +1,22 @@
+import styles from './Products.module.css';
+import { PRODUCTS } from '../../data';
+
+function Products() {
+  return (
+    <div id="products" className={styles.wrapper}>
+      {PRODUCTS.map((product, index) => (
+        <div key={index} className={styles.product}>
+          <div className={styles.icon}>
+            <img src={product.icon} alt="" aria-hidden="true" />
+          </div>
+          <div className={styles.content}>
+            <h3>{product.title}</h3>
+            <p>{product.text}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Products;
